57f0c8b56bb97fab2f4523785dc29759be9843e7
anurag22119 <154070051+anurag22119@users.noreply.github.com>
2023-12-17 20:02:39 +0530

    Add files via upload

---
+++ 01ass01.c
@@ -0,0 +1,20 @@
+//Write a C program to print your name, date of birth. and mobile number. 
+#include <stdio.h>
+int main() 
+ {
+	// Declare and initialize variables
+    char name[] = "Your Name:Anurag Verma";
+    char dob[] = "Date of Birth: 01/04/2003";
+    char mobile[] = "Mobile Number: +1234567890";
+// Print the information
+    printf("Name: %s\n", name);
+    printf("%s\n", dob);
+    printf("%s\n", mobile);
+return 0;
+}
+
+
+
+
+
+
